78d4c262c0a988c9065c485baea5ab1a12fd0c55
Khoa Vo <khoa.vo@hatch.live>
2020-12-21 15:26:11 +0200

    Added multi-arch image build

--- push.sh
+++ push.sh
@@ -1,4 +1,18 @@
-docker build -t nabsul/k8s-ecr-login-renew:$1 .
-docker build -t nabsul/k8s-ecr-login-renew:arm32v7-$1 -f Dockerfile-arm .
-docker push nabsul/k8s-ecr-login-renew:$1
-docker push nabsul/k8s-ecr-login-renew:arm32v7-$1
+VERSION=1.2
+REGISTRY=public.ecr.aws/s3r8h5m7
+IMAGE=${REGISTRY}/k8s-ecr-login-renew:${VERSION}
+
+for ARCH in amd64 arm64 arm
+do
+    docker build -f Dockerfile-${ARCH} -t ${IMAGE}-${ARCH} .
+    docker push ${IMAGE}-${ARCH}
+done
+
+docker manifest create ${IMAGE} ${IMAGE}-amd64 ${IMAGE}-arm64 ${IMAGE}-arm
+
+for ARCH in amd64 arm64 arm
+do
+    docker manifest annotate --arch ${ARCH} ${IMAGE} ${IMAGE}-${ARCH}
+done
+
+docker manifest push ${IMAGE}
